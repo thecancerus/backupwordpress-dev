39ef2fc677b54d15a19b214ea02eb35e5b55bfa9
Tom Willmot <tom@humanmade.co.uk>
2013-07-08 12:12:55 +0100

    Auto load backupwordpress and backupwordpress pro

--- content/plugins-mu/loader.php
+++ content/plugins-mu/loader.php
@@ -1,45 +1,26 @@
 <?php
 
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+add_filter( 'pre_option_active_plugins', function( $active_plugins ) {
 
-$loaded_plugins = array();
+	if ( empty( $active_plugins) )
+		$active_plugins = array();
 
-//Loop through all plugins in MU Plugins directory.
-//Note get_plugins requires path relative to plugins dir. 
-foreach ( get_plugins( '/../' . basename( __DIR__ ) ) as $plugin_path => $plugin ) {
+	if ( file_exists( plugin_dir_path( 'backupwordpress' ) ) )
+		$active_plugins[] = 'backupwordpress/backupwordpress.php';
 
-	// Only load HM Dev if HM_DEV is true.
-	if ( 'HM Dev' === $plugin['Name'] && ( ! defined( 'HM_DEV') || defined( 'HM_DEV' ) && false === HM_DEV ) )
-		continue;
 
-	include_once( WPMU_PLUGIN_DIR . '/' . $plugin_path );
+	foreach( array(
+		'backupwordpress-pro-dreamobjects',
+		'backupwordpress-pro-dropbox',
+		'backupwordpress-pro-ftp',
+		'backupwordpress-pro-gdrive',
+		'backupwordpress-pro-rackspace',
+		'backupwordpress-pro-s3',
+		'backupwordpress-pro-winazure'
+	) as $pro_plugin )
 
-	if ( $plugin['Name'] !== 'MU Plugins Loader' )
-		$loaded_plugins[] = $plugin['Name'];
-}
+		$active_plugins[] = 'backupwordpress-pro/' . $pro_plugin . '/' . $pro_plugin . '.php';
 
-if ( file_exists( 'tlc-transients/tlc-transients.php' ) ) {
-	$loaded_plugins[] = 'TLC Transients';
-	include_once( 'tlc-transients/tlc-transients.php' );
-}
+	return $active_plugins;
 
-if ( file_exists( WPMU_PLUGIN_DIR . '/custom-meta-boxes/custom-meta-boxes.php' ) ) {
-	$loaded_plugins[] = 'Custom Meta Boxes';
-	include_once( 'custom-meta-boxes/custom-meta-boxes.php' );
-}
-
-/*
-Plugin Name: MU Plugins Loader
-Description: Loaded the below plugins
-Author: Human Made Limited
-Version: 0.2
-Author URI: http://humanmade.co.uk/
-*/
-
-add_filter( 'plugin_row_meta', function( $plugin_meta, $plugin_file, $plugin_data, $status ) use ( $loaded_plugins ) {
-
-	if ( $plugin_file !== 'loader.php' )
-		return $plugin_meta;
-
-	return $loaded_plugins;
-}, 10, 4 );
+} );
